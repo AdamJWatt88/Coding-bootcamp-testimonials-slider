d5f2adad6f2d6097d4c4f9b72781af1ac619f9bc
Adam <adamjwatt88@gmail.com>
2020-06-25 11:51:13 -0700

    finish eventlisteners for prev/next

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-// store each person in objects
-// make function to create DOM
+// store each person in objects X
+// make function to create slides X
+// add forEach loop into slides function X
+// for each slide made push onto an array to keep track of shown slide and hidden slide X
+// when slide is slides[0] display in DOM. when slide is any other in array, add hide class to div.slide X
 // remove and add hide class to slides
-// add forEach loop into addToDOM function
-    // loop through people object
-// for each slide made push onto an array to keep track of shown slide and hidden slide
+// create functions for eventListeners on prev and next buttons to add hide and remove hide class
+
+
 
 
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
-const people = [
-    { 
-        quote: ' " ' + "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future." + ' " ',
+const people = [{
+    quote: ' " ' + "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future." + ' " ',
     name: "Tanya Sinclair",
     jobTitle: "UX Engineer",
     image: "images/image-tanya.jpg"
-    },
-    { 
-        quote: ' " ' + "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. " + ' " ',
+  },
+  {
+    quote: ' " ' + "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. " + ' " ',
     name: "John Tarkpor Junior ",
     jobTitle: "Front End Developer",
     image: "images/image-john.jpg"
-    }
+  },
+  {
+    quote: ' " ' + " Dis course is nice but no teach how to hunt buggos" + ' " ',
+    name: "Cheese",
+    jobTitle: "Buggo Hunter",
+    image: "images/cheese.jpeg"
+  }
 ]
 
+const slides = [];
 
-function addToDOM() {
+
+// loop through people and make a div.slide and push to slides
+function createSlides() {
+  people.forEach(person => {
     const div = document.createElement('div');
     div.classList.add('slide');
-    div.innerHTML = people.forEach( person => {
-        `
+    div.innerHTML =
+      `
         <div class="profile-container">
         <img class="profile-photo" src="${person.image}" alt="" />
         <div class="nav-container">
@@ -45,11 +57,49 @@ function addToDOM() {
         <p><span class="name">${person.name}</span> <span class="job-title">${person.jobTitle}</span></p>
       </div>
         `
-    })
+    slides.push(div)
+  })
 
-    document.body.appendChild(div);
+  addToDOM()
+}
+
+createSlides()
+
+// apply hide class to every slide that is NOT index of [0]
+function addToDOM() {
+  slides.forEach((slide, index) => {
+    document.body.appendChild(slide)
+    if (index !== 0) {
+      slide.classList.add('hide')
+    }
+  })
+}
+
+
+// if slide is index 0 go to slide -1. this is last slide. if slide is -1 then slide minus 1
+// each time classList(hide) needs to be removed 
+function prevSlide() {
+
+  if(document.body.querySelectorAll('.slide nth-of-type') === 0 ) {
+    classList.add('hide')
+  }
+
+
+  // document.body.querySelectorAll('.slide').forEach(slide => {
+  //   if (slide === document.body.querySelector('.slide' div:nth-child(1) )) {
+  //     slide.style.border = "5px solid green";
+  //   }
+  // })
 
 }
 
-addToDOM()
+// if slide is index 0 go to slide[1]. if slide is -1 then go to slide[0]
+// each time classList(hide) needs to be removed
+function nextSlide() {
 
+}
+
+prevSlide()
+
+document.querySelector('.prev').addEventListener('click', () => console.log('prev'))
+document.querySelector('.next').addEventListener('click', () => console.log('next'))
